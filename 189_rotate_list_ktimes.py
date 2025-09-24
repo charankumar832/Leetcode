b31d4bc5d6822b6bef32b666e091186cc60c522f
charankumar832 <charankumarreddyreddivari@gmail.com>
2025-09-24 23:56:10 +0530

    Add files via upload

---
+++ 189_rotate_list_ktimes.py
@@ -0,0 +1,9 @@
+class Solution:
+    def rotate(self, nums: list[int], k: int) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        n=len(nums)
+        r=k%n
+        
+        nums[:]=nums[n-r:]+nums[:n-r]
